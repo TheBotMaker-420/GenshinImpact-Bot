413dd635f9725935c38b7062399e5f5d5c6ccf16
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-07-16 01:54:47 +0530

    Added Data To Embeds

--- commands/information/characters.js
+++ commands/information/characters.js
@@ -14,15 +14,21 @@ module.exports = {
     async execute(interaction) {
         const id = interaction.options.getString('name');
 
+        let stars = "";
+
         // Below Code Returns True When The Name Is Found
         // let find= data.some(item => item.name.toLowerCase() === id.toLowerCase());
         // console.log(find);
 
         let find_one = data.find(item => item.name.toLowerCase() === id.toLowerCase());
 
+        for (let i=1;i<=find_one.quality;i++) {
+            stars += ":star: ";
+        }
+
         if (find_one) {
             const char_embed = new EmbedBuilder()
-                .setColor("Yellow")
+                .setColor(find_one.color)
                 .setTitle(`**__${find_one.name}__**`)
                 .addFields(
                     {
@@ -32,47 +38,47 @@ module.exports = {
                     },
                     {
                         name: "**Quality**",
-                        value: `${find_one.title}`,
+                        value: `${stars}`,
                         inline: true
                     },
                     {
                         name: "**Vision**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.vision}`,
                         inline: true
                     },
                     {
                         name: "**Model Type**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.model_type}`,
                         inline: true
                     },
                     {
                         name: "**Birthday**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.birthday}`,
                         inline: true
                     },
                     {
                         name: "**Region**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.region}`,
                         inline: true
                     },
                     {
                         name: "**Constellation**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.constellation}`,
                         inline: true
                     },
                     {
                         name: "**Weapon**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.weapon}`,
                         inline: true
                     },
                     {
                         name: "**Special Dish**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.special_dish}`,
                         inline: true
                     },
                     {
                         name: "**Release Date**",
-                        value: `${find_one.title}`,
+                        value: `${find_one.release_date}`,
                         inline: true
                     },
                 )
