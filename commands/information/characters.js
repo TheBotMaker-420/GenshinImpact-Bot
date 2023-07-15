dd6f1a6c374227f570f2345d769409086972eb7a
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-07-16 01:27:48 +0530

    Added Embed

--- commands/information/characters.js
+++ commands/information/characters.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const data = require('../../data/characters/data.json');
 
 module.exports = {
@@ -8,11 +8,82 @@ module.exports = {
         .addStringOption(option =>
             option
                 .setName('name')
+                .setDescription('Enter A Valid Name')
                 .setRequired(true)
         ),
-    async execute(interaction){
+    async execute(interaction) {
         const id = interaction.options.getString('name');
 
-        await interaction.reply(data[0].name);
+        // Below Code Returns True When The Name Is Found
+        // let find= data.some(item => item.name.toLowerCase() === id.toLowerCase());
+        // console.log(find);
+
+        let find_one = data.find(item => item.name.toLowerCase() === id.toLowerCase());
+
+        if (find_one) {
+            const char_embed = new EmbedBuilder()
+                .setColor("Yellow")
+                .setTitle(`**__${find_one.name}__**`)
+                .addFields(
+                    {
+                        name: "**Title**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Quality**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Vision**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Model Type**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Birthday**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Region**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Constellation**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Weapon**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Special Dish**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                    {
+                        name: "**Release Date**",
+                        value: `${find_one.title}`,
+                        inline: true
+                    },
+                )
+                .setDescription(find_one.description)
+                .setImage(find_one.image)
+
+            await interaction.reply({
+                embeds: [char_embed]
+            });
+        } else {
+            await interaction.reply("Character Not Found !");
+        }
     },
 };
