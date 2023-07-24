a26f54129382641309b013109cac9dc60f42ce3b
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-07-24 06:04:09 +0530

    Minor Fixes

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,9 +1,9 @@
-const { client, SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Ping!'),
+        .setDescription('Replies with Ping'),
     async execute(interaction) {
         const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
         await interaction.editReply(`RoundTrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
