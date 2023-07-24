e0e40bfa6facf49d97556a5ad7236238a216e393
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-07-24 05:50:28 +0530

    Added Support Command

---
+++ commands/utility/support.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('support')
+        .setDescription("Replies With Support Sever's Link"),
+    async execute(interaction) {
+        const support_embed = new EmbedBuilder()
+            .setColor("DarkGold")
+            .setTitle("**Support Server**")
+            .setDescription(`[Click Here To Join The Support Server](https://discord.gg/YeNZe3AgcQ)`)
+            .setTimestamp()
+            .setFooter({
+                text: `To Get More Help Type \`/help\`.`
+            })
+        await interaction.reply({
+            embeds: [support_embed]
+        });
+    },
+};
