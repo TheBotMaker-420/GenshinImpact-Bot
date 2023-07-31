b8e2102096953448b19287d8448c65c604af19cc
anmol420 <anmolanand2k15@gmail.com>
2023-08-01 00:27:25 +0530

    Minor Code Change

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -6,6 +6,6 @@ module.exports = {
         .setDescription('Replies with Ping'),
     async execute(interaction) {
         const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-        await interaction.editReply(`RoundTrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+        await interaction.editReply(`${interaction.client.ws.ping}ms`);
     },
 };
